b878679a8d881c2b995d77e86d75adebc53e8f8d
Jian Jie <contact.jianjie@gmail.com>
2015-07-22 17:24:28 +0800

    Remove template VCs and add VCs for NotesListView

---
+++ Paddy/NotesListViewController.h
@@ -0,0 +1,14 @@
+//
+//  NotesListViewController.h
+//  Paddy
+//
+//  Created by Jian Jie on 22/7/15.
+//  Copyright (c) 2015 Liau Jian Jie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "NSIndexPath+IndexPathToString.h"
+
+@interface NotesListViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+@end
---
+++ Paddy/NotesListViewController.m
@@ -0,0 +1,52 @@
+//
+//  NotesListViewController.m
+//  Paddy
+//
+//  Created by Jian Jie on 22/7/15.
+//  Copyright (c) 2015 Liau Jian Jie. All rights reserved.
+//
+
+#import "NotesListViewController.h"
+
+@interface NotesListViewController ()
+
+@end
+
+@implementation NotesListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITableView Datasource
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CELLIDENTIFIER = @"NotesCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CELLIDENTIFIER];
+    
+    if (!cell)
+    {
+        cell.textLabel.text = indexPath.displayableString;
+    }
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 25;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 44.0;
+}
+
+@end
